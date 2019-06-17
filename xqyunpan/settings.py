be4f12a3d6d459f5d6b7930b934608ae1232ae6e
liqiang <1206709430@qq.com>
2019-06-17 16:10:29 +0800

    1206709430@qq.com

--- xqyunpan/settings.py
+++ xqyunpan/settings.py
@@ -149,3 +149,13 @@ STATICFILES_DIRS=[
 #         },
 #     }
 # }
+
+# session配置
+SESSION_COOKIE_NAME = "sessionid"      #Session的cookie保存在浏览器上时的key
+SESSION_COOKIE_PATH = "/"              #Session的cookie保存的路径(默认)
+SESSION_COOKIE_DOMAIN = None            #Session的cookie保存的域名(默认)
+SESSION_COOKIE_SECURE = False           #是否Https传输cookie
+SESSION_COOKIE_HTTPONLY = True          #是否Session的cookie只支持http传输(默认)
+SESSION_COOKIE_AGE = 1209600            #Session的cookie失效日期(2周)(默认)
+SESSION_SAVE_EVERY_REQUEST = False      #是否每次请求都保存Session，默认修改之后才能保存
+SESSION_COOKIE_AT_BROWSER_CLOSE = True  #是否设置关闭浏览器使得Session过期
